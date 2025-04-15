f8576ac2ef06d791bfd7707858f3036929fc805d
CaiPeterJiazhen <1528596603@qq.com>
2025-04-15 20:00:13 +0800

    4.15NewUpdate

--- Notebook.py
+++ Notebook.py
@@ -1,1 +1,4 @@
-print("你好")
+from pathlib import Path
+
+path = Path("PackagePractice")
+print(path.exists())  #检查路径是否存在
--- PackagePractice/app.py
+++ PackagePractice/app.py
@@ -7,5 +7,6 @@ from ecommerce import shipping #  # 导入 ecommerce 包当中的shipping 模块
 
 ecommerce.shipping.calc_shipping()
 #调用 ecommerce Package 当中shipping.py 模块的 calc_shipping 函数
-calc_shipping
+
+calc_shipping()
 shipping.calc_shipping() 
